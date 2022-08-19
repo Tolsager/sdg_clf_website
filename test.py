9f7bef3c7698ff394dcc9377f3c641eb5e9382f7
Victor Tolsager Olesen <victortolsagerolesen1@gmail.com>
2022-08-19 11:59:01 +0200

    setup post request to local host

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
-
+import requests
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    prediction = None
     if request.method == "POST":
         sdg_text = request.form["sdg_text"]
-    return render_template("home.html")
+        url = "http://127.0.0.1:8000/predict"
+        post_obj = {"text": sdg_text}
+        prediction = requests.post(url, json=post_obj)
+        prediction = json.loads(prediction.content)["prediction"]
+    return render_template("home.html", prediction=prediction)
 
 @app.route("/about")
 def about():
