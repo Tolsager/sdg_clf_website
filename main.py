4b5f1875cd57d622b0cf898535d529112180dff4
Tolsager <victortolsagerolesen1@gmail.com>
2022-08-24 00:18:45 +0200

    change url to azure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import json
+
 import requests
 from flask import Flask, render_template, request
-import json
-import random
+
 app = Flask(__name__)
 
 
@@ -10,15 +11,13 @@ def home():
     prediction = None
     if request.method == "POST":
         sdg_text = request.form["sdg_text"]
-        url = "http://192.168.1.27:8080/predict"
+        url = "https://sdg-clf-api-free.azurewebsites.net/predict"
         post_obj = {"text": sdg_text}
         prediction = requests.post(url, json=post_obj)
         prediction = json.loads(prediction.content)["prediction"]
     return render_template("home.html", prediction=prediction)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
